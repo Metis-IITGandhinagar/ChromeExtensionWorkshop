b40ccec3851d7eb4cce385d15585d53c3a7be3fa
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2025-02-08 02:44:20 +0530

    refactored

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,21 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json" assert { type: "json" };
 
 export default defineConfig({
-  plugins: [
-    crx({
-      manifest,
-      build: {
-        rollupOptions: {
-          input: {
-            popup: "popup.html",
-            dialog: "dialog.html",
-          },
-          output: {
-            // Add this to preserve CSS class names
-            assetFileNames: "assets/[name].[ext]",
-          },
-        },
-      },
-    }),
-  ],
+	plugins: [
+		crx({
+			manifest,
+			build: {
+				rollupOptions: {
+					input: {
+						popup: "sidebar/sidebar.html",
+						dialog: "dialog/dialog.html",
+					},
+					output: {
+						// Add this to preserve CSS class names
+						assetFileNames: "assets/[name].[ext]",
+					},
+				},
+			},
+		}),
+	],
 });
